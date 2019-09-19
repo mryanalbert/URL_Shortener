ba5c187682c51b5499629f47436b0a4e5ebdf0f6
Ryan <mryanalbert@gmail.com>
2019-09-18 23:03:51 -0700

    Changed gitignore and index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 const dns = require('dns');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mryanalbert:JE1otipbTbM9YlKG@cluster0-x2hka.mongodb.net/test?retryWrites=true&w=majority');
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test');
 // local database - mongodb://localhost:27017/test, { useNewUrlParser: true }
 
 let Schema = mongoose.Schema;
